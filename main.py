81c6d57a5fa480a62563ec96c110cd0f28830533
박재욱 <80030233+Mesopotamia2957@users.noreply.github.com>
2022-07-30 20:23:11 +0900

    [feat] 매일 특정시간에 알림이 가는 기능 추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import crawling, kakaotalk
-
+from datetime import datetime
+import schedule
 kakao_enterprise = "https://careers.kakaoenterprise.com/go/Category_All/546844/"
 kakao_bank = "https://recruit.kakaobank.com/jobs"
 kakao_games = "https://kakaogames.recruiter.co.kr/app/jobnotice/list"
@@ -13,15 +14,30 @@ pschool = "https://www.patentschool.co.kr/post/list.php?g=mo&cate=1" # 윤태웅
 service_data = [{'user': '박재욱', 'company': ['kakao_enterprise'],            'chat_name': "박재욱"},
                 {'user': '윤태웅', 'company': ['pschool'],                     'chat_name': "변리사스쿨 모의고사 알림"}]
 
-if __name__ == '__main__':
+date = datetime.today()
+
+
+
+def function():
     for data in service_data:
         company = data['company']
         print(company)
-    for c in company:
-        result = crawling.webpage(c)
 
-    message = ''
-    # for info in result:
-    #     temp = ('기업 : ' + info['company'] + '\n' + '공고명 : ' + info['title'] + '\n' + '링크 : ' + info['url'] + '\n\n')
-    #     message += temp
-    # kakaotalk.send(chat_name, message)
+        for c in company:
+            result = crawling.webpage(c)
+
+        message = '%s 정보입니다.\n' % date
+        for info in result:
+            if data['chat_name'] == "변리사스쿨 모의고사 알림":
+                temp = ('게시글 : ' + info['title'] + '\n' + '링크 : ' + info['url'] + '\n\n')
+                message += temp
+            else:
+                temp = ('기업 : ' + info['company'] + '\n' + '공고명 : ' + info['title'] + '\n' + '링크 : ' + info['url'] + '\n\n')
+                message += temp
+
+        kakaotalk.send(data['chat_name'], message)
+
+if __name__ == '__main__':
+    schedule.every().day.at("20:19").do(function)
+    while True:
+        schedule.run_pending()
