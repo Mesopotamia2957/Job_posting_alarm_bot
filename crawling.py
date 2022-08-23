468fb63bfd74ee1a0b3be390aae7a2eaf847c1b0
박재욱 <80030233+Mesopotamia2957@users.noreply.github.com>
2022-08-23 15:48:15 +0900

    [fix] 응답값 예외처리 및 데이터 형식 변경중

--- crawling.py
+++ crawling.py
@@ -2,57 +2,67 @@ import requests, re
 from bs4 import BeautifulSoup
 
 target_dic = [
-            {'name': '카카오 엔터프라이즈', 'link': "https://careers.kakaoenterprise.com/go/Category_All/546844/"},
-            {'name': '변리사스쿨 모의고사', 'link': "https://www.patentschool.co.kr/post/list.php?g=mo&cate=1"}
+            {'company': 'kakao_enterprise', 'name': '카카오 엔터프라이즈', 'link': "https://careers.kakaoenterprise.com/go/Category_All/546844/"},
+            {'company': 'pschool', 'name': '변리사스쿨 모의고사', 'link': "https://www.patentschool.co.kr/post/list.php?g=mo&cate=1"}
              ]
 
-def webpage():
+def webpage(c):
     result = []
     for info in target_dic:
-        if info['name'] == '카카오 엔터프라이즈':
+        if info['company'] == 'kakao_enterprise':
             name = info['name']
             link = info['link']
             resp = requests.get(link)
-            resp.raise_for_status()
-            html = resp.text
-            soup = BeautifulSoup(html, "lxml")
-
-            table_tag = soup.find('table', attrs={"class": "searchResults full table table-striped table-hover"})
-            a_tag = table_tag.find_all('a')
-            temp = []
-            for value in a_tag:
-                if value not in temp:
-                    temp.append(value)
-
-            for text in temp:
-                info = {}
+            if resp.status_code != 200:
                 info['company'] = name
-                info['title'] = text.get_text()
-                info['url'] = "https://careers.kakaoenterprise.com" + text.get("href")
+                info['title'] = '데이터를 불러올 수 없습니다.'
+                info['url'] = "해당 링크가 정상적으로 응답하지 않습니다" + "https://careers.kakaoenterprise.com"
                 result.append(info)
+            else:
+                html = resp.text
+                soup = BeautifulSoup(html, "lxml")
 
-        elif info['name'] == '변리사스쿨 모의고사':
-            name = info['name']
-            link = info['link']
-            resp = requests.get(link)
-            resp.raise_for_status()
-            html = resp.text
-            soup = BeautifulSoup(html, "lxml")
+                table_tag = soup.find('table', attrs={"class": "searchResults full table table-striped table-hover"})
+                a_tag = table_tag.find_all('a')
+                temp = []
+                for value in a_tag:
+                    if value not in temp:
+                        temp.append(value)
 
-            ul_tag = soup.find('ul', attrs={"class": "board_list notice"})
-            a_tag = ul_tag.find_all('a')
-            temp = []
-            for value in a_tag:
-                if value not in temp:
-                    temp.append(value)
-
-            for text in temp:
-                info = {}
-                if "시행안내" in text.get_text():
+                for text in temp:
+                    info = {}
                     info['company'] = name
                     info['title'] = text.get_text()
-                    info['url'] = text.get("href")
+                    info['url'] = "https://careers.kakaoenterprise.com" + text.get("href")
                     result.append(info)
+
+        elif info['company'] == 'pschool':
+            name = info['name']
+            link = info['link']
+            resp = requests.get(link)
+            if resp.status_code != 200:
+                info['company'] = name
+                info['title'] = '데이터를 불러올 수 없습니다.'
+                info['url'] = "해당 링크가 정상적으로 응답하지 않습니다" + "https://careers.kakaoenterprise.com"
+                result.append(info)
+            else:
+                html = resp.text
+                soup = BeautifulSoup(html, "lxml")
+
+                ul_tag = soup.find('ul', attrs={"class": "board_list notice"})
+                a_tag = ul_tag.find_all('a')
+                temp = []
+                for value in a_tag:
+                    if value not in temp:
+                        temp.append(value)
+
+                for text in temp:
+                    info = {}
+                    if "시행안내" in text.get_text():
+                        info['company'] = name
+                        info['title'] = text.get_text()
+                        info['url'] = text.get("href")
+                        result.append(info)
 
     return result
 
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import crawling, kakaotalk
 from datetime import datetime
-import schedule
-kakao_enterprise = "https://careers.kakaoenterprise.com/go/Category_All/546844/"
-kakao_bank = "https://recruit.kakaobank.com/jobs"
-kakao_games = "https://kakaogames.recruiter.co.kr/app/jobnotice/list"
-naver = "https://recruit.navercorp.com/rcrt/list.do?annoId=&sw=&subJobCdArr=1010004%2C1030001%2C1030002%2C1060001&sysCompanyCdArr=&empTypeCdArr=&entTypeCdArr=&workAreaCdArr="
-line = "https://careers.linecorp.com/ko/jobs?ca=Engineering&ci=Seoul&co=East%20Asia"
-kepco = "https://recruit.kepco.co.kr:444/frt/frt0001/list.do"
-pschool = "https://www.patentschool.co.kr/post/list.php?g=mo&cate=1" # 윤태웅 변리사스쿨 모의고사
+import schedule, time
 
+target_dic = [
+            {'company': 'kakao_enterprise', 'name': '카카오 엔터프라이즈', 'link': "https://careers.kakaoenterprise.com/go/Category_All/546844/"},
+            {'company': 'pschool', 'name': '변리사스쿨 모의고사', 'link': "https://www.patentschool.co.kr/post/list.php?g=mo&cate=1"}
+             ]
 
 
 service_data = [{'user': '박재욱', 'company': ['kakao_enterprise'],            'chat_name': "박재욱"},
@@ -27,6 +24,7 @@ def function():
             result = crawling.webpage(c)
 
         message = '%s 정보입니다.\n' % date
+
         for info in result:
             if data['chat_name'] == "변리사스쿨 모의고사 알림":
                 temp = ('게시글 : ' + info['title'] + '\n' + '링크 : ' + info['url'] + '\n\n')
@@ -39,6 +37,7 @@ def function():
 
 if __name__ == '__main__':
     function()
-    # schedule.every().day.at("20:19").do(function)
+    # schedule.every().day.at("09:00").do(function)
     # while True:
     #     schedule.run_pending()
+    #     time.sleep(1)
